e8d9eaa7f81520b8e8a282649691649ac625aed6
Momona-Wada <momona.9410@gmail.com>
2025-01-11 16:09:08 -0800

    update button design

--- js/script.js
+++ js/script.js
@@ -14,6 +14,9 @@ document.addEventListener("DOMContentLoaded", () => {
         const input = document.getElementById("numberInput").value
         const errorMessage = document.getElementById("messageContainer")
 
+        gameManager.clearButtons()
+        gameManager.clearMessages()
+
         if (gameManager.validator.isValidInput(Number(input))) {
             errorMessage.textContent = ""
             gameManager.start(Number(input))
@@ -39,42 +42,49 @@ class Validator {
 }
 
 class Button {
-    constructor(number){
+    constructor(number, color){
         this.number = number
-        this.element = this.createButton()
+        this.color = color
+        this.element = this.createButton(color)
     }
 
-    createButton() {
+    createButton(color) {
         const button = document.createElement("button")
-        button.style.height = "5rem"
+        button.style.height = "5em"
         button.style.width = "10em"
-        button.style.backgroundColor = this.getRandomColor()
+        button.style.backgroundColor = color
+        button.textContent = this.number
 
         const buttonContainer = document.getElementById("buttonContainer")
         buttonContainer.appendChild(button)
 
         return button
     }
-
-    getRandomColor() {
-        const colors = ["red", "blue", "yellow", "green", "purple", "pink", "orange"]
-        const randomIndex = Math.floor(Math.random() * colors.length)
-        console.log(colors[randomIndex])
-        return colors[randomIndex]
-    }
 }
 
 class GameManager {
     constructor() {
         this.validator = new Validator(MIN_BUTTONS, MAX_BUTTONS)
+        this.originalColors = [
+            "#FFC1CC", 
+            "#A7C7E7", 
+            "#B2F2BB", 
+            "#E6E6FA", 
+            "#FFDAB9", 
+            "#FFFACD", 
+            "#FCE883"  
+            ]
+        this.colors = [...this.originalColors]
         this.buttons = []
     }
 
     start(numberButtons) {
         this.clearButtons()
 
+        this.colors = [...this.originalColors]
+
         for (let i = 1; i <= numberButtons; i++) {
-            const button = new Button(i)
+            const button = new Button(i, this.getRandomColor())
             this.buttons.push(button)
         }
     }
@@ -84,6 +94,20 @@ class GameManager {
         buttonContainer.innerHTML = ""
         this.buttons = []
     }
+
+    clearMessages() {
+        const messageContainer = document.getElementById("messageContainer")
+        messageContainer.innerHTML = ""
+    }
+
+    getRandomColor() {
+        const randomIndex = Math.floor(Math.random() * this.colors.length)
+        const color = this.colors[randomIndex]
+        this.colors.splice(randomIndex, 1)
+        return color
+    }
+
+    
 }
 
 
