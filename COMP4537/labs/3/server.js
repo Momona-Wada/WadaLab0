e12fee79b2e7f0e8d8a0fad83338328b4f03b216
Momona-Wada <144402784+Momona-Wada@users.noreply.github.com>
2025-02-01 02:16:19 -0800

    change error code

--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -21,7 +21,7 @@ module.exports = (req, res) => {
         res.writeHead(200, {"Content-Type": "text/html"})
         res.end(`<div style="color: blue;">${responseMessage}</div>`)
     } else {
-        res.writeHead(400, {"Content-Type": "text/html"})
+        res.writeHead(404, {"Content-Type": "text/html"})
         res.end("404 Not Found")
     }
 }
