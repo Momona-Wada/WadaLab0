5a6abc577b63bc55bb15fc2bf9e7d37c1bfec197
Momona-Wada <144402784+Momona-Wada@users.noreply.github.com>
2025-02-01 21:55:48 -0800

    Implement writeFile.js

---
+++ COMP4537/api/labs/3/lang/messages/en/user.js
@@ -0,0 +1,5 @@
+export const messages = {
+    TEXT_NOT_PROVIDED: "Error: No text provided to append.",
+    TEXT_APPENDED_SUCCESSFULLY: "Successfully appended text to file.txt",
+    INTERNAL_SERVER_ERROR: "Internal Server Error"
+}
---
+++ COMP4537/api/labs/3/writeFile.js
@@ -0,0 +1,46 @@
+const fs = require("fs")
+const path = require("path")
+const url = require("url")
+const { messages } = require("./lang/messages/en/user")
+
+
+class FileWriter {
+    constructor(filename) {
+        this.filePath = path.resolve(__dirname, filename);
+    }
+
+    appendText(text) {
+        return new Promise((resolve, reject) => {
+            fs.appendFile(this.filePath, text + "\n", (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve()
+                }
+            })
+        })
+        
+    }
+}
+
+module.exports = async (req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    const text = parsedUrl.query.text;
+
+    if (!text) {
+        res.writeHead(400, messages.TEXT_NOT_PROVIDED);
+        return;
+    }
+
+    const fileWriter = new FileWriter("file.txt");
+    
+    try {
+        await fileWriter.appendText(text);
+        res.writeHead(200, {"Content-Type": "text.html"});
+        res.end(messages.TEXT_APPENDED_SUCCESSFULLY);
+    } catch {
+        res.writeHead(500, {"Context-Type": "text/html"});
+        res.end(messages.INTERNAL_SERVER_ERROR);
+    }
+    
+}
