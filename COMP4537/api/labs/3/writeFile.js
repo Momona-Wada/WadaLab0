6f51dc35b2f14e12fe1df895dc31917d8152e8c3
Momona-Wada <144402784+Momona-Wada@users.noreply.github.com>
2025-02-01 22:21:22 -0800

    try without using message

--- COMP4537/api/labs/3/writeFile.js
+++ COMP4537/api/labs/3/writeFile.js
@@ -1,8 +1,61 @@
-const fs = require("fs")
-const path = require("path")
-const url = require("url")
-const { messages } = require("./lang/messages/en/user")
+// const fs = require("fs")
+// const path = require("path")
+// const url = require("url")
+// const { messages } = require("./lang/messages/en/user")
 
+// const PORT = process.env.PORT || 3000
+
+// class FileWriter {
+//     constructor(filename) {
+//         this.filePath = path.resolve(__dirname, filename);
+//     }
+
+//     appendText(text) {
+//         return new Promise((resolve, reject) => {
+//             fs.appendFile(this.filePath, text + "\n", (err) => {
+//                 if (err) {
+//                     reject(err);
+//                 } else {
+//                     resolve()
+//                 }
+//             })
+//         })
+        
+//     }
+// }
+
+// module.exports = async (req, res) => {
+//     const parsedUrl = url.parse(req.url, true);
+//     const text = parsedUrl.query.text;
+
+//     if (!text) {
+//         res.writeHead(400, messages.TEXT_NOT_PROVIDED);
+//         return;
+//     }
+
+//     const fileWriter = new FileWriter("file.txt");
+    
+//     try {
+//         await fileWriter.appendText(text);
+//         res.writeHead(200, {"Content-Type": "text/html"});
+//         res.end(messages.TEXT_APPENDED_SUCCESSFULLY);
+//     } catch {
+//         res.writeHead(500, {"Content-Type": "text/html"});
+//         res.end(messages.INTERNAL_SERVER_ERROR);
+//     }
+    
+// }
+
+// // if (process.env.NODE_ENV !== 'production') {
+// //     const PORT = process.env.PORT || 3000;
+// //     http.createServer(module.exports).listen(PORT, () => {
+// //         console.log(`Server is running on port ${PORT}`);
+// //     });
+// // }
+
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
 
 class FileWriter {
     constructor(filename) {
@@ -15,11 +68,10 @@ class FileWriter {
                 if (err) {
                     reject(err);
                 } else {
-                    resolve()
+                    resolve();
                 }
-            })
-        })
-        
+            });
+        });
     }
 }
 
@@ -28,19 +80,19 @@ module.exports = async (req, res) => {
     const text = parsedUrl.query.text;
 
     if (!text) {
-        res.writeHead(400, messages.TEXT_NOT_PROVIDED);
+        res.writeHead(400, { "Content-Type": "text/html" });
+        res.end("Error: No text provided to append.");
         return;
     }
 
     const fileWriter = new FileWriter("file.txt");
-    
+
     try {
         await fileWriter.appendText(text);
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(messages.TEXT_APPENDED_SUCCESSFULLY);
-    } catch {
-        res.writeHead(500, {"Content-Type": "text/html"});
-        res.end(messages.INTERNAL_SERVER_ERROR);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("Successfully appended text to file.txt");
+    } catch (err) {
+        res.writeHead(500, { "Content-Type": "text/html" });
+        res.end("Internal Server Error");
     }
-    
-}
+};
