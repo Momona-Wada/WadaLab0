635566e833e730cc9562d08e0681c57a92c299e8
Momona-Wada <144402784+Momona-Wada@users.noreply.github.com>
2025-02-02 00:35:25 -0800

    undo

--- COMP4537/api/labs/3/readFile.js
+++ COMP4537/api/labs/3/readFile.js
@@ -1,64 +1,35 @@
-// const fs = require("fs");
-// const path = require("path");
-// const { messages } = require("./lang/messages/en/user");
+const fs = require("fs");
+const path = require("path");
+const { messages } = require("./lang/messages/en/user");
 
-// class FileReader {
-//     constructor(filename) {
-//         this.filePath = path.join("/tmp", filename);
-//     }
+class FileReader {
+    constructor(filename) {
+        this.filePath = path.join("/tmp", filename);
+    }
 
-//     readFile() {
-//         return new Promise((resolve, reject) => {
-//             fs.readFile(this.filePath, "utf8", (err, data) => {
-//                 if (err) {
-//                     reject(err);
-//                 } else {
-//                     resolve(data)
-//                 }
-//             })
-//         })
-//     }
-// }
-
-// module.exports = async (req, res) => {
-//     const fileReader = new FileReader("file.txt")
-
-//     try {
-//         const data = await fileReader.readFile();
-//         res.writeHead(200, {"Content-Type": "text/html"});
-//         res.end(`<p>${data}</p>`);
-//     } catch(err) {
-//         console.log("Error reading file:", err)
-//         res.writeHead(404, {"Content-Type": "text/html"});
-//         res.end(messages.FILE_NOT_FOUND);
-//     }
-// }
-
-const { initializeApp } = require("firebase/app");
-const { getFirestore, doc, getDoc } = require("firebase/firestore");
-
-// Firebase設定
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+    readFile() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.filePath, "utf8", (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+    }
+}
 
 module.exports = async (req, res) => {
-    try {
-        const docRef = doc(db, "files", "file1");
-        const docSnap = await getDoc(docRef);
+    const fileReader = new FileReader("file.txt")
 
-        if (docSnap.exists()) {
-            res.status(200).send(`File content: ${docSnap.data().content}`);
-        } else {
-            res.status(404).send("File not found.");
-        }
-    } catch (error) {
-        console.error("Error reading from Firestore:", error);
-        res.status(500).send("Internal Server Error");
+    try {
+        const data = await fileReader.readFile();
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(`<p>${data}</p>`);
+    } catch(err) {
+        console.log("Error reading file:", err)
+        res.writeHead(404, {"Content-Type": "text/html"});
+        res.end(messages.FILE_NOT_FOUND);
     }
-};
+}
--- COMP4537/api/labs/3/writeFile.js
+++ COMP4537/api/labs/3/writeFile.js
@@ -1,79 +1,48 @@
-// console.log("Loading modules...");
-// const fs = require("fs")
-// const path = require("path")
-// const url = require("url")
-// const { messages } = require("./lang/messages/en/user")
+console.log("Loading modules...");
+const fs = require("fs")
+const path = require("path")
+const url = require("url")
+const { messages } = require("./lang/messages/en/user")
 
 
-// class FileWriter {
-//     constructor(filename) {
-//         this.filePath = path.join("/tmp", filename);
-//         console.log("File path resolved to:", this.filePath);
-//     }
+class FileWriter {
+    constructor(filename) {
+        this.filePath = path.join("/tmp", filename);
+        console.log("File path resolved to:", this.filePath);
+    }
 
-//     appendText(text) {
-//         return new Promise((resolve, reject) => {
-//             fs.appendFile(this.filePath, text + "\n", (err) => {
-//                 if (err) {
-//                     reject(err);
-//                 } else {
-//                     resolve()
-//                 }
-//             })
-//         })
+    appendText(text) {
+        return new Promise((resolve, reject) => {
+            fs.appendFile(this.filePath, text + "\n", (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve()
+                }
+            })
+        })
         
-//     }
-// }
-
-// module.exports = async (req, res) => {
-//     const parsedUrl = url.parse(req.url, true);
-//     const text = parsedUrl.query.text;
-
-//     if (!text) {
-//         res.writeHead(400, messages.TEXT_NOT_PROVIDED);
-//         return;
-//     }
-
-//     const fileWriter = new FileWriter("file.txt");
-    
-//     try {
-//         await fileWriter.appendText(text);
-//         res.writeHead(200, {"Content-Type": "text/html"});
-//         res.end(messages.TEXT_APPENDED_SUCCESSFULLY);
-//     } catch (err) {
-//         console.error("Error appending text:", err)
-//         res.writeHead(500, {"Content-Type": "text/html"});
-//         res.end(messages.INTERNAL_SERVER_ERROR);
-//     }
-// }
-
-const { initializeApp } = require("firebase/app");
-const { getFirestore, doc, setDoc } = require("firebase/firestore");
-
-// Firebase設定
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+    }
+}
 
 module.exports = async (req, res) => {
-    const text = req.query.text;
+    const parsedUrl = url.parse(req.url, true);
+    const text = parsedUrl.query.text;
 
     if (!text) {
-        res.status(400).send("No text provided.");
+        res.writeHead(400, messages.TEXT_NOT_PROVIDED);
         return;
     }
 
+    const fileWriter = new FileWriter("file.txt");
+    
     try {
-        // Firestoreにテキストを保存
-        await setDoc(doc(db, "files", "file1"), { content: text });
-        res.status(200).send("Text successfully saved to Firestore.");
-    } catch (error) {
-        console.error("Error saving to Firestore:", error);
-        res.status(500).send("Internal Server Error");
+        await fileWriter.appendText(text);
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(messages.TEXT_APPENDED_SUCCESSFULLY);
+    } catch (err) {
+        console.error("Error appending text:", err)
+        res.writeHead(500, {"Content-Type": "text/html"});
+        res.end(messages.INTERNAL_SERVER_ERROR);
     }
-};
+}
